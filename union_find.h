18e63794b131403dc2f5d9723c0419746fc65b15
Venkatasai <venkatasaikgp102@gmail.com>
2022-01-27 17:48:14 +0530

    coded and verified for union_find.c

--- union_find.h
+++ union_find.h
@@ -1,12 +1,20 @@
-#include "llist.h"
-typedef LLIST NODE_PTR; // Making a union find data structure
+typedef struct ND_PTR
+{
+    int data;
+    int cnt_mems;
+    struct ND_PTR *next;
+} ND_PTR;
+
+typedef ND_PTR* NODE_PTR;
 
 typedef struct uf
 {
     int n;
     int size;
     NODE_PTR *set_uf;
-} UNION_FIND;
+} UN_FD;
+
+typedef UN_FD* UNION_FIND;
 
 UNION_FIND createUF(int n); // Create a union find data structure that can store n nodes pointers
 UNION_FIND makeSetUF(UNION_FIND F, int x, int *i); // Inserts a value x
