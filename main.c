b13a4445d112e72a9c231c5db7117d99bd336a06
Venkata Sai Suvvari <venkatasaikgp102@gmail.com>
2022-02-05 02:21:17 +0530

    need to write the make for static and check make for both

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "graph.h"
+
+int main(int argc, char const *argv[])
+{
+    printf("Enter the name of text file: ");
+    char *pth = (char *)malloc(sizeof(char) * 30);
+    scanf("%s", pth);
+    GRAPH gr = readGraph(pth);
+    DFS(gr);
+    BFS(gr);
+    MST(gr);
+    return 0;
+}
