9870b95da01b393d9bbb735831291e5b0373cfd9
Venkata Sai Suvvari <venkatasaikgp102@gmail.com>
2022-02-06 02:18:38 +0530

    final 2

--- 20CS10067_ASGN1/libraries/llist.h
+++ 20CS10067_ASGN1/libraries/llist.h
@@ -1,3 +1,6 @@
+#ifndef LLIST_H
+#define LLIST_H
+
 typedef struct NODE
 {
     int data;
@@ -14,3 +17,5 @@ LLIST insertAtEnd(LLIST H, int k); // Inserts a node at the end of list
 LLIST deleteFromFront(LLIST H, int *k); // Delete a node at the front of list
 LLIST deleteFromEnd(LLIST H, int *k); // Delete a node at the end of list
 LLIST deleteList(LLIST H, int k); // Deletes the first occurance of node with value k
+
+#endif
--- 20CS10067_ASGN1/libraries/stack.h
+++ 20CS10067_ASGN1/libraries/stack.h
@@ -1,3 +1,6 @@
+#ifndef STACK_H
+#define STACK_H
+
 typedef LLIST STACK; // Making a stack data structure
 
 STACK createStack(); // Creates an empty stack
@@ -5,3 +8,5 @@ int isEmptyStack(STACK S); // Returns 1 if stack is empty
 int top_stack(STACK S); // Returns the value in the top of the stack
 STACK push(STACK S, int k); // Push a node to stack
 STACK pop(STACK S, int *k); // Pop a node from stack
+
+#endif
